acd58098b7f80e0717bf4d4f81c152ad5eaf49fe
Taahirah Denmark <126827805+Engineernoob@users.noreply.github.com>
2025-08-11 19:31:50 -0500

    adding proxy

--- origin-frontend/next.config.ts
+++ origin-frontend/next.config.ts
@@ -1,11 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     domains: ["images.unsplash.com"],
   },
-
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3000/:path*", // backend on port 3000
+      },
+    ];
+  },
 };
 
 export default nextConfig;
